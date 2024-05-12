9b16e6f5309204b0d5ce6748782eddef366d9d2d
Vita <75259415+Vitacci@users.noreply.github.com>
2024-05-12 19:44:16 +0300

    Update train.cpp

--- src/train.cpp
+++ src/train.cpp
@@ -6,33 +6,55 @@ Train::Train() {
   cntOp = 0;
 }
 
+Train::~Train() {
+  Cage* current = first;
+  while (current->nezt != first) {
+    Cage* temp = current;
+    current = current->next;
+    delete temp;
+  }
+}
+
 void Train::addCage(bool light) {
   Cage *newCage = new Cage;
   newCage->light = light;
-  if (!first) {
+  newCage->next = nullptr;
+  if (first == nullptr) {
+    newCage->prev = nullptr;
     first = newCage;
     first->next = first;
-    first->prev = first;
   } else {
-    Cage *last = first->prev;
-    last->next = newCage;
-    newCage->prev = last;
+    Cage* current = first;
+    while (current->next != first) {
+      current = current->next;
+    }
+    current->next = newCage;
+    newCage->prev = current;
     newCage->next = first;
     first->prev = newCage;
   }
 }
 
 int Train::getLength() {
-  if (!first) {
-    return 0;
-  }
   int len = 0;
   Cage *current = first;
-  do {
-    len++;
+  current->light = true;
+  while (true) {
     current = current->next;
-  } while (current != first);
-  return len;
+    cntOp++;
+    len++;
+    if (current->light == true) {
+      current->light = false;
+      int mlen = len;
+      len = 0;
+      for (int i = 0; i < mlen; i++) {
+        current = current->prev;
+        cntOp++;
+      }
+      if (current->light == false)
+        return mlen;
+    }
+  }
 }
 
 int Train::getOpCount() {
