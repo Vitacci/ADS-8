73828a04c3307730ee1b51a99e15c4e91d5f3f72
Vita <75259415+Vitacci@users.noreply.github.com>
2024-05-12 19:31:06 +0300

    Update train.cpp

--- src/train.cpp
+++ src/train.cpp
@@ -1,2 +1,40 @@
 // Copyright 2021 NNTU-CS
 #include "train.h"
+
+Train::Train() {
+  first = nullptr;
+  cntOp = 0;
+}
+
+void Train::addCage(bool light) {
+  Cage *newCage = new Cage;
+  newCage->light = light;
+  if (!first) {
+    first = newCage;
+    first->next = first;
+    first->prev = first;
+  } else {
+    Cage *last = first->prev;
+    last->next = newCage;
+    newCage->prev = last;
+    newCage->next = first;
+    first->prev = newCage;
+  }
+}
+
+int Train::getLenght() {
+  if (!first) {
+    return 0;
+  }
+  int len = 0;
+  Cage *current = first;
+  do {
+    len++;
+    current = current->next;
+  } while (current != first);
+  return len;
+}
+
+int Train::getOpCount() {
+  return cntOp;
+}
